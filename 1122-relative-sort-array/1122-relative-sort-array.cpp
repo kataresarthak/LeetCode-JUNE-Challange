214bc9a6b436a424c5621a4149f88b130937c351
Sarthak R. Katare <kataresarthak001@gmail.com>
2024-06-13 20:00:52 +0530

    Time: 5 ms (32.90%), Space: 10.1 MB (44.35%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -3,13 +3,16 @@ public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
 
         unordered_map<int, int> map;
-        for (int num : arr1) {
+        for (int num : arr1)
+        {
             map[num]++;
         }
+        
         vector<int> result;
         for (int num : arr2)
         {
-            while (map[num] > 0) {
+            while (map[num] > 0)
+            {
                 result.push_back(num);
                 map[num]--;
             }
@@ -20,11 +23,13 @@ public:
         {
             int num = entry.first;
             int count = entry.second;
-            while (count>0) {
+            while (count>0)
+            {
                 element.push_back(num);
                 count--;
             }
         }
+        
         sort(element.begin(), element.end());
         result.insert(result.end(), element.begin(), element.end());
         return result;
