5573b8d6baa2cdaeb99838103634e074e8a2ff4d
Sarthak R. Katare <kataresarthak001@gmail.com>
2024-06-16 09:24:27 +0530

    Time: 4 ms (67.05%), Space: 13.8 MB (45.41%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+
+        long miss = 1;
+        int i = 0, patches = 0;
+        
+        while (miss <= n)
+        {
+            if (i < nums.size() && nums[i] <= miss) {
+                miss += nums[i];
+                i++;
+            }
+            else {
+                miss += miss;
+                patches++;
+            }
+        }
+        return patches;
+    }
+};
