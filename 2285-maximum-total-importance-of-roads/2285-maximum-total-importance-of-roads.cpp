94f277ca48f62613f506aaa978f259af2a464876
Sarthak R. Katare <kataresarthak001@gmail.com>
2024-06-28 19:04:06 +0530

    Time: 245 ms (87.59%), Space: 116.5 MB (85.00%) - LeetHub

---
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long maximumImportance(int n, vector<vector<int>>& roads) {
+        vector<int> degree(n, 0);
+        for (auto& road : roads) {
+            degree[road[0]]++;
+            degree[road[1]]++;
+        }
+        sort(degree.begin(), degree.end());
+        long long ans = 0;
+        for (int i = 0; i < n; i++) 
+            ans += (long long)(i + 1) * degree[i];
+        return ans;
+    }
+};
